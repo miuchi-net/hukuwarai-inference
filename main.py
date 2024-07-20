e78c82a39fab9888f8b84b213da53f00d3e54f25
abap34 <abap0002@gmail.com>
2024-07-20 12:59:40 +0900

    レスポンスの型明確にした

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import uuid
 
 import numpy as np
 import uvicorn
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
 from PIL import Image as PILImage
 from pydantic import BaseModel
@@ -23,9 +23,18 @@ class SimilarityRequest(BaseModel):
     img2: Image
     model_name: str
 
+class SimilarityResponse(BaseModel):
+    similarity: float
+
+
+class RenderRequest(BaseModel):
+    html_src: str
+    css_src: str
+
+class RenderResponse(BaseModel):
+    image_path: str
 
 app = FastAPI()
-
 
 async def html_to_image(html_content: str, css_content: str, output_path: str):
     browser = await launch()
@@ -46,25 +55,22 @@ async def html_to_image(html_content: str, css_content: str, output_path: str):
 
 
 @app.post("/render")
-async def render(request: Request):
-    data = await request.json()
-    html_content = data.get("html")
-    css_content = data.get("css")
-
-    if not html_content:
-        raise HTTPException(status_code=400, detail="HTML content is required")
-    if not css_content:
-        css_content = ""
+async def render(request: RenderRequest) -> RenderResponse:
+    html_content = request.html_src
+    css_content = request.css_src
 
     output_path = f"rendered_{uuid.uuid4()}.png"
 
-    await html_to_image(html_content, css_content, output_path)
+    try:
+        await html_to_image(html_content, css_content, output_path)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Failed to render image: " + str(e))
 
-    return FileResponse(output_path, media_type="image/png", filename="output.png")
+    return RenderResponse(image_path=output_path)
 
 
 @app.post("/similarity")
-async def similarity(request: SimilarityRequest):
+async def similarity(request: SimilarityRequest) -> SimilarityResponse:
     img1_data = base64.b64decode(request.img1.data)
     img2_data = base64.b64decode(request.img2.data)
 
@@ -91,8 +97,10 @@ async def similarity(request: SimilarityRequest):
         model = PtPretrainedModel(request.model_name)
 
     imgsim = ImageSim(img1, img2, model)
-    return {"similarity": imgsim.calculate()}
+    
+    similarity = imgsim.calculate()
 
+    return SimilarityResponse(similarity=similarity)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
