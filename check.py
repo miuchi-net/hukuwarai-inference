91a933bacfb1e659b533ccab0209b7428e620021
abap34 <abap0002@gmail.com>
2024-07-20 23:04:42 +0900

    boudingbox のチェッカを実装

--- check.py
+++ check.py
@@ -1,11 +1,12 @@
+import argparse
 import base64
+import concurrent.futures
 import io
+import time
 import uuid
+
 import requests
 from PIL import Image
-import argparse
-import time
-import concurrent.futures
 
 
 def image_to_base64(image_path):
@@ -44,6 +45,22 @@ def render(html_content, css_content):
         raise Exception("Failed to render image: " + response.text)
 
 
+def boundingbox(html_content, css_content):
+    url = "http://127.0.0.1:8000/boudingbox"
+
+    payload = {
+        "html_src": html_content,
+        "css_src": css_content,
+    }
+
+    response = requests.post(url, json=payload)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception("Failed to render image: " + response.text)
+
+
 def palette(image_path, max_colors):
     url = "http://127.0.0.1:8000/palette"
 
@@ -61,25 +78,38 @@ def run_task(action, args, index):
 
     if action == "similarity":
         if len(args) != 3:
-            result =  "Usage for compare: check.py similarity <image1> <image2> <model_name>"
+            result = (
+                "Usage for compare: check.py similarity <image1> <image2> <model_name>"
+            )
         else:
             image1, image2, model_name = args
             result = similarity(image1, image2, model_name)
     elif action == "render":
-        if len(args) < 1 or len(args) > 2:
-            result =  "Usage for render: check.py render <html_content> [css_content]"
+        if len(args) != 2:
+            result = "Usage for render: check.py render <html_content> <css_content>"
         else:
-            html_path = args[0]
+            html_path, css_path = args
             with open(html_path, "r") as f:
                 html_content = f.read()
 
-            css_content = ""
-            if len(args) == 2:
-                css_path = args[1]
-                with open(css_path, "r") as f:
-                    css_content = f.read()
+            with open(css_path, "r") as f:
+                css_content = f.read()
 
-            result =  render(html_content, css_content)
+            result = render(html_content, css_content)
+
+    elif action == "boundingbox":
+        if len(args) != 2:
+            result = "Usage for boundingbox: check.py boundingbox <html_content> <css_content>"
+        else:
+            html_path, css_path = args
+            with open(html_path, "r") as f:
+                html_content = f.read()
+
+            with open(css_path, "r") as f:
+                css_content = f.read()
+
+            result = boundingbox(html_content, css_content)
+
     elif action == "palette":
         if len(args) != 2:
             result = "Usage for palette: check.py color_picker <image> <max_colors>"
@@ -93,7 +123,7 @@ def run_task(action, args, index):
 
             img_path = f"palette_{uuid.uuid4()}.png"
             img.save(img_path)
-            result =  f"Saved palette image to {img_path}"
+            result = f"Saved palette image to {img_path}"
     else:
         raise ValueError("Invalid action")
 
@@ -101,24 +131,33 @@ def run_task(action, args, index):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Image similarity, HTML to image conversion, and color picker")
-    parser.add_argument("action", type=str, choices=["similarity", "render", "palette"], help="Action to perform")
+    parser = argparse.ArgumentParser(
+        description="Image similarity, HTML to image conversion, and color picker"
+    )
+    parser.add_argument(
+        "action",
+        type=str,
+        choices=["similarity", "render", "boundingbox", "palette"],
+        help="Action to perform",
+    )
     parser.add_argument("args", nargs="*", help="Arguments for the action")
 
     args = parser.parse_args()
 
     start_time = time.time()
 
-    N_PROCESSES = 100
-    N_PARALLEL = 100
+    N_PROCESSES = 1
+    N_PARALLEL = 1
 
     with concurrent.futures.ProcessPoolExecutor(max_workers=N_PROCESSES) as executor:
-        futures = [executor.submit(run_task, args.action, args.args, i) for i in range(N_PARALLEL)]
+        futures = [
+            executor.submit(run_task, args.action, args.args, i)
+            for i in range(N_PARALLEL)
+        ]
         for future in concurrent.futures.as_completed(futures):
             result, elapsed_time = future.result()
             print(f"Result: {result}")
             print(f"Elapsed time: {elapsed_time:.2f} seconds")
             print()
-    
+
     print(f"Total elapsed time: {time.time() - start_time:.2f} seconds")
-        
