d788a9114648c63faa6f0e629e9163a75bbd40a2
abap34 <abap0002@gmail.com>
2024-07-20 23:04:31 +0900

    boundingbox を実装

--- src/routers/render_router.py
+++ src/routers/render_router.py
@@ -30,7 +30,7 @@ class Renderer:
     async def html_to_image(
         self, html_content: str, css_content: str, output_path: str
     ):
-        async with self.semaphore:  # Use semaphore to limit concurrency
+        async with self.semaphore:  
             page = await self.browser.newPage()
             try:
                 src = f"""
@@ -48,6 +48,66 @@ class Renderer:
                 await page.setViewport({"width": 500, "height": 500})
                 await page.setContent(src)
                 await page.screenshot({"path": output_path})
+            finally:
+                await page.close()
+
+    async def boundingbox(self, html_content: str, css_content: str, output_path: str):
+        async with self.semaphore:  
+            page = await self.browser.newPage()
+            try:
+                src = f"""
+                <html>
+                <head>
+                    <style>{css_content}</style>
+                </head>
+                <body>
+                    {html_content}
+                </body>
+                </html>
+                """
+                await page.setJavaScriptEnabled(False)
+                await page.setOfflineMode(True)
+                await page.setViewport({"width": 500, "height": 500})
+                await page.setContent(src)
+                # body 以下の全ての要素のbounding boxを取得する
+                elements = await page.querySelectorAll('body *')
+                print(f'--- Found {len(elements)} elements ---')
+                for element in elements:
+                    box = await element.boundingBox()
+                    print('Box:', box)
+                    if box is None:
+                        continue
+                    # bounding boxを描画して、元の div を削除
+                    await page.evaluate(
+                        """
+                        (box) => {
+                            const div = document.createElement('div');
+                            div.style.position = 'absolute';
+                            div.style.border = '1px solid red';
+                            div.style.left = box.x + 'px';
+                            div.style.top = box.y + 'px';
+                            div.style.width = box.width + 'px';
+                            div.style.height = box.height + 'px';
+                            document.body.appendChild(div);
+                        }
+                        """,        
+                        box,
+                    )
+
+                # element を削除 (ここの for を分ける必要があることに注意)
+                for element in elements:    
+                    await page.evaluate(
+                        """
+                        (element) => {
+                            element.remove();
+                        }
+                        """,
+                        element,
+                    )
+
+                await page.screenshot({"path": output_path})
+    
+
             finally:
                 await page.close()
 
@@ -70,6 +130,20 @@ async def render(request: RenderRequest) -> RenderResponse:
 
     return RenderResponse(image_path=output_path)
 
+
+@render_router.post("/boudingbox")
+async def boundingbox(request: RenderRequest) -> RenderResponse:
+    html_content = request.html_src
+    css_content = request.css_src
+
+    output_path = f"rendered_{uuid.uuid4()}.png"
+
+    try:
+        await renderer.boundingbox(html_content, css_content, output_path)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Failed to render image: " + str(e))
+
+    return RenderResponse(image_path=output_path)
 
 @asynccontextmanager
 async def lifespan(app):
